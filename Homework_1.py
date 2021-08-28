9524fb91b5fc8d94829321a7e6bac0d97f0e5b4f
Chris <cwhitney32@gmail.com>
2021-08-28 06:27:45 -0700

    checked, corrected, commented

--- Homework_1.py
+++ Homework_1.py
@@ -3,16 +3,21 @@ from scipy.optimize import minimize
 from scipy.optimize import Bounds
 from scipy.optimize import LinearConstraint
 
-##bounds=Bounds([-10,10],[-10,10],[-10,10],[-10,10],[-10,10])
+##bounds
 bounds=Bounds([-10,-10,-10,-10,-10],[10,10,10,10,10])
 
-linear_constraint = LinearConstraint([[1,3,0,0,0],[0,0,1,1,-2],[0,1,0,0,-1]],[1,1,1], [1,1,1])
+##constraints
+linear_constraint = LinearConstraint([[1,3,0,0,0],[0,0,1,1,-2],[0,1,0,0,-1]],[0,0,0], [0,0,0])
 
+#funtion 
 def f(x):
-    return (x[0]-x[1]**2 + (x[1]+x[2]-2)**2 + (x[3]-1)**2+(x[4]-1)**2)
+    return (x[0]-x[1])**2 + (x[1]+x[2]-2)**2 + (x[3]-1)**2+(x[4]-1)**2
 
-x0 = np.array([1, 2, 3, 4, 5])
+#inintial conditions
+x0 = np.array([.1, -20, 3, 4000, 50])
 
+#minimization 
 res = minimize(f, x0,constraints=linear_constraint, bounds=bounds)
 
+#print results 
 print(res)
