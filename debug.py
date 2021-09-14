2ee1ff50c38dcff3b08f30bcaa182127ac632d3a
Chris <cwhitney32@gmail.com>
2021-09-14 06:13:15 -0700

    updates

--- debug.py
+++ debug.py
@@ -3,28 +3,39 @@ import matplotlib.pyplot as plt
 from line_search_bt import *
 
 x_i=np.array([1,1])
+x_init=x_i
 convergence_criteria=.0001
-alpha=1
+alpha=.0001
+conv=1
 error=1
 err=[]
 itt=[]
 count=1
 
 def func(x):
-    return 5*x[0]**2+12*x[0]*x[1]-8*x[1]+10*x[1]**2-14*x[1]+5
+    return 5*x[0]**2+12*x[0]*x[1]-8*x[0]+10*x[1]**2-14*x[1]+5
 
 def gradient(x):
    return np.array([10*x[0]+12*x[1]-8,12*x[0]+20*x[1]-14])
 
-while error > convergence_criteria:
+while conv > convergence_criteria:
+
     x_k=x_i
+
+    alpha=line_search_bt(x_i,func,gradient,alpha,.3,.5)
+    
     x_i=x_k-alpha*gradient(x_k)
+
     error=abs(max(x_i-x_k))
+
+    conv=np.linalg.norm(gradient(x_i))
+    
     err.append(float(error))
 
-    alpha=line_search_bt(x_i,func,gradient,alpha,.8,.8)
+  
 
 x=np.array([-2*x_i[0]-3*x_i[1]+1,x_i[0],x_i[1]])
+
 print(x)
 
 plt.plot(err)
--- line_search_bt.py
+++ line_search_bt.py
@@ -2,22 +2,25 @@ import numpy as np
 
 
 def line_search_bt(x,func,grad,alpha,t,rho):
-    
    
+    alpha=1
+
     direction=-grad(x)
-   
 
-    in1=x+alpha*np.matmul(np.transpose(grad(x)),direction)
+    
+    f1=func(x+alpha*direction)
 
-    f1=func(in1)
     f2=func(x)+t*alpha*np.matmul(np.transpose(grad(x)),direction)
     
     while f1>f2:
-        in1=x+alpha*np.matmul(np.transpose(grad(x)),direction)
-        f1=func(in1)
+
+     
+        f1=func(x+alpha*direction)
+
         f2=func(x)+t*alpha*np.matmul(np.transpose(grad(x)),direction)
+    
         alpha=alpha*rho
-        print(alpha)
+    
 
     return alpha
     
