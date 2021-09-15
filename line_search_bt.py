020b32ad909f09e43d0876628a1a0c300a7c4482
Chris <cwhitney32@gmail.com>
2021-09-15 07:21:53 -0700

    corrected direction for gd and N

--- line_search_bt.py
+++ line_search_bt.py
@@ -1,17 +1,14 @@
 import numpy as np
 
 
-def line_search_bt(x,func,grad,alpha,t,rho):
+def line_search_bt(x,func,grad,alpha,t,rho,direction):
    
     alpha=1
 
-    direction=-grad(x)
-
-    
     f1=func(x+alpha*direction)
 
     f2=func(x)+t*alpha*np.matmul(np.transpose(grad(x)),direction)
-    
+
     while f1>f2:
 
      
