02c546af016540fbec91508049618e7ced6018a5
Chris <cwhitney32@gmail.com>
2021-10-14 00:14:56 -0700

    update after office hours

--- line_search_bt.py
+++ line_search_bt.py
@@ -7,14 +7,13 @@ def line_search_bt(x,func,grad,alpha,t,rho,direction):
 
     f1=func(x+alpha*direction)
 
-    f2=func(x)+t*alpha*np.matmul(np.transpose(grad(x)),direction)
+    f2=func(x)+t*alpha*np.matmul(np.transpose(grad),direction)
 
     while f1>f2:
 
-     
         f1=func(x+alpha*direction)
 
-        f2=func(x)+t*alpha*np.matmul(np.transpose(grad(x)),direction)
+        f2=func(x)+t*alpha*np.matmul(np.transpose(grad),direction)
     
         alpha=alpha*rho
     
